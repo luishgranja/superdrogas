be55d2af672ad3272b2065ae64599e7cbe4814c4
Jhon Hadder Diaz <jhon.hadder.diaz@correounivalle.edu.co>
2019-06-25 23:57:32 -0500

    feature: products created

---
+++ apps/products/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from .models import Product
+
+
+class ProductSerialize(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ('name',
+                  'description',
+                  'price',
+                  'category',
+                  'sku',
+                  'unit',
+                  'weight',
+                  'is_active',
+                  'brand')
--- superdrogas/urls.py
+++ superdrogas/urls.py
@@ -3,9 +3,11 @@ from rest_framework import routers
 
 # Apps viewsets
 from apps.users.viewsets import UserViewSet
+from apps.products.viewsets import ProductViewSet
 
 router = routers.DefaultRouter()
 
 router.register(r'users', UserViewSet)
+router.register(r'products', ProductViewSet)
 
 urlpatterns = router.urls
