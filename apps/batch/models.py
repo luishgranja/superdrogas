319677f8d16c44be9c8edd1deb9037709d2dbfcf
Sebastián Villegas <jhoan.villegas@correounivalle.edu.co>
2019-07-02 17:37:13 -0500

    feature:foreign key from product in batch

--- apps/batch/models.py
+++ apps/batch/models.py
@@ -1,8 +1,8 @@
 from django.db import models
-
+from apps.products.models import Product
 
 class Batch(models.Model):
-    product = models.CharField(max_length=20)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.IntegerField()
     manufacturing_date = models.DateField()
     expiration_date = models.DateField()
