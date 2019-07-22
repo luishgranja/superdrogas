72748a2adfb3a306c9be0a943491a32edd55d104
Jose Vargas <jose.mamian@correounivalle.edu.co>
2019-07-22 11:10:23 -0500

    feat: add landing page component

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import home from './modules/home'
+import landing from './modules/landing'
 
 Vue.use(Router)
 
@@ -8,7 +9,8 @@ const router = new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
-    { ...home }
+    { ...home },
+    { ...landing }
   ]
 })
 
