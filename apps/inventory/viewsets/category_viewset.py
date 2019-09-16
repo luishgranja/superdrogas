05107b706a7afd7470433d59567abe930e5bfd03
Sebastian Villegas <jhoan.villegas@correounivalle.edu.co>
2019-09-16 00:14:32 -0500

    feat: analytics

--- apps/inventory/viewsets/category_viewset.py
+++ apps/inventory/viewsets/category_viewset.py
@@ -9,7 +9,7 @@ from rest_framework import viewsets
 
 # Inventory models
 from apps.inventory.models import Category
-
+import requests
 # Inventory serializers
 from apps.inventory.serializers import CategorySerializer
 
@@ -29,3 +29,10 @@ class CategoryViewSet(viewsets.ModelViewSet):
         """
         instance.is_active = not instance.is_active
         instance.save()
+
+    def perform_create(self, serializer):
+        category = serializer.save()
+        payload = 'v=1&t=pageview&tid=UA-148049900-1&cid=c76c5a95-406f-4ed8-8c59-096da9f0cc36'
+        r = requests.post('http://www.google-analytics.com/collect', data=payload)
+        print (r)
+
